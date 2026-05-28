4cb5ea62fb36eb57383a26eb54caa2d740d626a4
Gerwen Jacob Gaceta <gerwenjacob@gmail.com>
2026-05-28 17:14:42 +0800

    Category Controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Category\StoreCategoryRequest;
+use App\Services\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+   
+    public function __construct(
+        protected CategoryService $categoryService
+    ) {}
+
+    public function index(Request $request)
+    {
+        return response()->json(
+            $this->categoryService->getCategories(
+                $request->only('search')
+            )
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        return response()->json(
+            $this->categoryService->createCategory(
+                $request->validated()
+            )
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return response()->json(
+            $this->categoryService->getCategory($id)
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreCategoryRequest $request, string $id)
+    {
+        return response()->json(
+            $this->categoryService->updateCategory($id, $request->validated())
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return response()->json([
+            'success' => $this->categoryService->deleteCategory($id)
+        ]);
+    }
+}
